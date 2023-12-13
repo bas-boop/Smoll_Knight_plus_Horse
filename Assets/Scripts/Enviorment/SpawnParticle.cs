54fccfd2bd208d2a9fa5c84042ec179dcb6b6dab
bas-boop <dereusbas461@gmail.com>
2023-12-13 19:13:57 +0100

    Particles for good and bad QTE

---
+++ Assets/Scripts/Enviorment/SpawnParticle.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+namespace Baz_geluk9.HKU
+{
+    public sealed class SpawnParticle : MonoBehaviour
+    {
+        [SerializeField] private ParticleSystem[] particles;
+
+        public void SpawnParticles(int particleToSpawn)
+        {
+            ParticleSystem instantiate = Instantiate(particles[particleToSpawn], gameObject.transform);
+            instantiate.transform.position = transform.position;
+        }
+    }
+}
